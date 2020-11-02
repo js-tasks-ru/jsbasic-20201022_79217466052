fd8a0c3aaabbb323409208f317e855c5aedf1924
Lidiya Chastikova <79217466052@yandex.ru>
2020-11-02 16:07:36 +0300

    + module 2

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -6,12 +6,10 @@ let calculator = {
   },
 
   sum() {
-    console.log(this.a + this.b)
     return(this.a + this.b);
   },
 
   mul() {
-    console.log(this.a * this.b)
     return(this.a * this.b);
   },
 }
